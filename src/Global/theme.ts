1757335154a6cce549572dea4d22f87274547609
Geraldosbn <geraldosbn92@gmail.com>
2023-04-19 00:14:44 -0300

    cleaning project

---
+++ src/Global/theme.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle, DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    primary: '',
+    text: '#61dafb',
+    background: '#282c34',
+  },
+};
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.text};
+    font-family: Arial, Helvetica, sans-serif;
+  }
+`;
