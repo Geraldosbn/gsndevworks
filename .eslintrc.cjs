ebb60e5dd4d5eb1c2b2b338776dbc6c67e472f58
geraldosbn <geraldosbn92@gmail.com>
2023-07-08 12:34:22 -0300

    corrigindo o eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,32 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react-hooks/recommended'
+    'plugin:react/recommended'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json'],
-    tsconfigRootDir: __dirname
+    ecmaVersion: 'latest'
   },
-  plugins: ['react-refresh'],
+  plugins: ['@typescript-eslint', 'react'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true }
-    ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off'
+    // '@typescript-eslint/no-non-null-assertion': 'off',
+    // '@typescript-eslint/no-unsafe-assignment': 'off'
   }
 }
